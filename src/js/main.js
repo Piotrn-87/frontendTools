4cfa9e12cbd323e1d8bde56984b0caf91fa1d5f4
Piotr Napieralski <piotrn-87@o2.pl>
2019-06-28 17:50:05 +0200

    Update frontend tools

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello world 🚀'); 
+const headerText = document.querySelector(".hero__header--text");
+const text = "Frontend Resources !!"
+
+const time = 200;
+let index = 0;
+
+const addLetter = () => {
+    headerText.textContent += text[index];
+    index++;
+    if (index === text.length) {
+        clearInterval(headerTyping);
+    }
+}
+
+const headerTyping = setInterval(addLetter, time);
