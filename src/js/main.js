b6bc86011dad2ff0e51cab33af1a9ad34a85c9fb
Piotr Napieralski <piotrn-87@o2.pl>
2019-06-30 15:17:27 +0200

    Feature: adding backdrop to mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 const headerText = document.querySelector(".hero__header--text");
 const burgerMenu = document.querySelector(".burger");
-const burgerIcon = document.querySelector(".fa-bars");
-const xIcon = document.querySelector(".fa-times");
-const fas = document.querySelector(".fas");
 const mobileNav = document.querySelector(".mobile-nav");
+const backdrop = document.querySelector(".backdrop");
+const activeElement = document.querySelectorAll(".active");
 
 const text = "Frontend Resources !!"
 const time = 200;
@@ -18,8 +17,15 @@ const addLetter = () => {
 }
 const headerTyping = setInterval(addLetter, time);
 
-burgerMenu.addEventListener("click", function () {
-    burgerIcon.classList.toggle("show_icon");
-    xIcon.classList.toggle("show_icon");
-    mobileNav.classList.toggle("mobile-nav__visible");
+burgerMenu.addEventListener("click", (e) => {
+    for (let i = 0; i < activeElement.length; i++) {
+        activeElement[i].classList.toggle("show_icon");
+        activeElement[i].classList.toggle("mobile-nav__visible");
+        activeElement[i].classList.toggle("backdrop-active");
+    }
+})
+
+backdrop.addEventListener("click", (e) => {
+    mobileNav.classList.remove("mobile-nav__visible");
+    backdrop.classList.remove("backdrop-active");
 })
